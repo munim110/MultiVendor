a5f793eed0b3c9427513ace66e5477466c309525
munim110 <munim987654321@gmail.com>
2024-03-17 23:48:59 +0600

    Added signup functionality

--- Auth/views.py
+++ Auth/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 # Create your views here.
+
+def signin(request):
+    return render(request, 'auth/login.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm-password')
+
+        if password != confirm_password:
+            return HttpResponse('Passwords do not match')
+        
+        new_user = User.objects.create_user(username=username, email=email, password=password)
+
+        if new_user is None:
+            return HttpResponse('User not created')
+        
+        new_user.first_name = firstname
+        new_user.last_name = lastname
+        new_user.save()
+
+        # next class will be about how to handle errors
+        # and redirecting to view functions rather than rendering templates
+        return HttpResponse('User created successfully')
+
+    return render(request, 'auth/signup.html')
--- MultiVendor/settings.py
+++ MultiVendor/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'MultiVendor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MultiVendorDB',
+        'USER' : 'root',
+        'PASSWORD' : '11701011',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
     }
 }
 
