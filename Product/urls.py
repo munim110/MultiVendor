069c7fa69e41497a587f87fcb6ec5d8dd6a19b05
munim110 <munim987654321@gmail.com>
2024-03-20 23:37:16 +0600

    Added new html templates

--- Product/urls.py
+++ Product/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('add/', addProduct, name='add_product'),
 ]
--- Product/views.py
+++ Product/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def addProduct(request):
+    return render(request, 'product/add_product.html')
